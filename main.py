05631e59dfb76e3ec15a9636b73707a3f538e886
kap1yn <124692729+kap1yn@users.noreply.github.com>
2023-08-07 14:30:14 +0300

    main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_key_letters():
     all_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', " ", '"']
 
     random.shuffle(all_letters)
     return all_letters
